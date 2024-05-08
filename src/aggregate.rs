08782b82cb2b594d6ebadc1fe8fcfb87cee63f1b
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-05-08 23:18:17 +0200

    Print results in main

--- src/aggregate.rs
+++ src/aggregate.rs
@@ -3,11 +3,16 @@ use std::sync::mpsc::Receiver;
 
 use crate::record::Record;
 
-pub fn reduce(rx: Receiver<HashMap<String, Record>>) {
-    let mut hmap = HashMap::new();
+pub fn reduce(rx: Receiver<HashMap<String, Record>>) -> Vec<(String, Record)> {
+    let mut hmap: HashMap<String, Record> = HashMap::new();
     while let Ok(stats) = rx.recv() {
-        for (city, rec) in stats {
-            hmap.entry(city).or_insert(Record::default()).merge(rec);
+        for (city, new) in stats {
+            hmap.entry(city)
+                .and_modify(|exist| exist.merge(&new))
+                .or_insert(new);
         }
     }
+    let mut v = hmap.into_iter().collect::<Vec<_>>();
+    v.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+    v
 }
