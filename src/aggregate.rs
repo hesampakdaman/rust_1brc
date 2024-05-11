bdf2af99421044b72b5cd38d4d4303308109bdc4
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-05-11 20:52:03 +0200

    Add fxhash

--- src/aggregate.rs
+++ src/aggregate.rs
@@ -1,16 +1,16 @@
 use crate::record::Record;
-use std::collections::HashMap;
+use fxhash::FxHashMap;
 use std::sync::mpsc::Receiver;
 
-pub fn reduce(rx: Receiver<HashMap<String, Record>>) -> Vec<(String, Record)> {
-    let mut hmap = HashMap::with_capacity(10_000);
+pub fn reduce(rx: Receiver<FxHashMap<String, Record>>) -> Vec<(String, Record)> {
+    let mut hmap = FxHashMap::default();
     while let Ok(stats) = rx.recv() {
         merge_records(&mut hmap, stats);
     }
     to_sorted_vec(hmap)
 }
 
-fn merge_records(dst: &mut HashMap<String, Record>, src: HashMap<String, Record>) {
+fn merge_records(dst: &mut FxHashMap<String, Record>, src: FxHashMap<String, Record>) {
     for (city, new_record) in src {
         dst.entry(city)
             .and_modify(|existing_record: &mut Record| existing_record.merge(&new_record))
@@ -18,7 +18,7 @@ fn merge_records(dst: &mut HashMap<String, Record>, src: HashMap<String, Record>
     }
 }
 
-fn to_sorted_vec(hmap: HashMap<String, Record>) -> Vec<(String, Record)> {
+fn to_sorted_vec(hmap: FxHashMap<String, Record>) -> Vec<(String, Record)> {
     let mut v = hmap.into_iter().collect::<Vec<_>>();
     v.sort_unstable_by(|a, b| a.0.cmp(&b.0));
     v
