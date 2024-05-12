afbbcee369f1c5b367276224cf6ce43440a02aed
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2024-05-12 12:24:20 +0200

    Same for agg

--- src/aggregate.rs
+++ src/aggregate.rs
@@ -1,23 +1,23 @@
-use crate::weather::{Report, Station};
+use crate::weather;
 use std::sync::mpsc::Receiver;
 
-pub fn reduce(rx: Receiver<Report>) -> Vec<Station> {
-    let mut hmap = Report::default();
+pub fn reduce(rx: Receiver<weather::Report>) -> Vec<weather::Station> {
+    let mut hmap = weather::Report::default();
     while let Ok(stats) = rx.recv() {
         merge_records(&mut hmap, stats);
     }
     to_sorted_vec(hmap)
 }
 
-fn merge_records(dst: &mut Report, src: Report) {
+fn merge_records(dst: &mut weather::Report, src: weather::Report) {
     for (city, new_record) in src.into_iter() {
         dst.entry(city)
-            .and_modify(|existing_record: &mut Station| existing_record.merge(&new_record))
+            .and_modify(|existing_record: &mut weather::Station| existing_record.merge(&new_record))
             .or_insert(new_record);
     }
 }
 
-fn to_sorted_vec(hmap: Report) -> Vec<Station> {
+fn to_sorted_vec(hmap: weather::Report) -> Vec<weather::Station> {
     let mut v = hmap.to_vec();
     v.sort_unstable();
     v
